42706e02f88643508c291524401544cd4572688f
dkhade323@gmail.com <dkhade323@gmail.com>
2024-09-20 12:54:18 +0530

    add this

---
+++ Takinginputfromuser.py
@@ -0,0 +1,18 @@
+# Taking input from user & printing it .
+
+# 1. string input
+Field = input("Field :")
+print(Field) 
+
+
+# 2. int input
+age = int(input("age :"))
+print(age)
+
+
+# 3. Float input
+
+Fees = float(input("Fees :"))
+print(Fees)
+
+# print("my name is",name "and i am",age,"years old")
