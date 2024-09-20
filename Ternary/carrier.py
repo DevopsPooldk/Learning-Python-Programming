1a774b71ba8accb53f87fa4e70572ae88fb3d59a
dkhade323@gmail.com <dkhade323@gmail.com>
2024-09-20 18:17:57 +0530

    conditions

---
+++ Ternary/carrier.py
@@ -0,0 +1,2 @@
+carrier =input("enter Intrest :")
+print("Excited") if carrier == "Devops" or carrier == "hfts" else "not intrested"
---
+++ Ternary/cleverif/sal.py
@@ -0,0 +1,6 @@
+# this program of clever if : 
+# salary and tax
+
+sal = float(input("enter your salary :"))
+tax= sal*(0.1,0.2) [sal>=50000] # in this condition if contidtion geting true then printing 0.2 tax in output else 0.1 print
+print(tax)
