0cc705c8a3fb3b6f2dac8ec80320f01d9c0c84bf
dkhade323@gmail.com <dkhade323@gmail.com>
2024-09-24 15:18:43 +0530

    add new files

---
+++ TypeConversion/typecasting.py
@@ -0,0 +1,22 @@
+# This is forcefuly conversion
+
+# Ex. 1
+
+a=10.2
+b=52
+c= int(a)# int conversion
+print(c+b)
+
+# Ex 2.
+
+var1 =float("2.0") #float conversion
+var2 =20
+print(var1+var2)
+print(type(var1))
+
+
+# only type 
+
+a=3.145
+a=str(a)
+print(type(a))
---
+++ TypeConversion/typeconversion.py
@@ -0,0 +1,7 @@
+# This conversion automattically happen on the basis of language 
+
+#ex. 1
+a=2
+b=3.2
+sum=a+b
+print("sum :",sum)
