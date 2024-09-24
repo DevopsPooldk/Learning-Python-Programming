4d165a73adb0db9173a3a87dfb965aa443065478
dkhade323@gmail.com <dkhade323@gmail.com>
2024-09-24 15:59:15 +0530

    practice

---
+++ Practicequestions/Areaofsquare.py
@@ -0,0 +1,7 @@
+# WAP to input side of sqaure and print its area
+
+side1 = int(input("enter side one :"))
+side2 =int (input("enter side second :"))
+AreaOfSquare = (side1*side2)
+print("Area of Square :",AreaOfSquare)
+
---
+++ Practicequestions/sumof2num.py
@@ -0,0 +1,8 @@
+# write a program to input 2 numbers and print their Addition
+# integer input
+
+num1 = int(input("enter first no :"))
+num2 = int(input("enter second no :"))
+sum= num1+num2
+print("sum of entred no :",sum)
+print(type(sum))
