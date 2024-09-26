2c7d75c9495076c976bd25ef72a7757be4e07a7a
dkhade323@gmail.com <dkhade323@gmail.com>
2024-09-26 16:38:14 +0530

    add new examples

---
+++ String/Personagenamelen.py
@@ -0,0 +1,5 @@
+# WAP take a name input from user and print length of input
+
+name =input("enter Your name :")
+name= len(name) #leg function
+print(name)
---
+++ String/countexample$.py
@@ -0,0 +1,3 @@
+# Wap to find the occurrence of '$' in a String.
+str="my networth now $ $0 but one one its $1 unlimited zeros"
+print(str.count("$")) #count function
