9243f6366b8c4152e9dd85bc1ef30ea866bdf13e
dkhade323@gmail.com <dkhade323@gmail.com>
2024-10-02 16:22:29 +0530

    add new codes

---
+++ Practicequestions/sortmethod.py
@@ -0,0 +1,5 @@
+# Store the above values in a list and sort them from "A" to "D"
+
+store=["C","D","A","A","B","B","A"]
+store.sort()
+print(store)
---
+++ Practicequestions/tupelseofstudent.py
@@ -0,0 +1,4 @@
+# WAP to count the number of students with the "a" grade in the following tuple.
+
+tup=("a","c","b","a","c","a")
+print(tup.count("a"))# count function
